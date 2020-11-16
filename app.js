95e4a5839cd17d12aa286299648197260e371504
adit <aditiaaldama221@gmail.com>
2020-11-17 00:30:43 +0800

    CRUD Stories

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const path = require('path')
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -19,13 +20,39 @@ connectDB();
 
 const app = express();
 
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+)
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Handlebars Helpers
+const { formatDate, truncate, stripTags, editIcon, select } = require('./helpers/hbs')
+
 // Handlebars view engine
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: { formatDate, truncate, stripTags, editIcon, select },
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -42,12 +69,19 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set global var
+app.use(function(req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 5000;
 
